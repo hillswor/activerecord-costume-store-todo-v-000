a8e4a8b8ba458b66b55baa974c3e2b617118da08
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-11-25 21:22:57 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -5,7 +5,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.string :name
       t.integer :price
       t.string :image_url
-      t.integer :size
+      t.string :size
       t.timestamps
     end
   end
